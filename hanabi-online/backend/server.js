1b9a2f767b7d09a0b3b44f6a20dca8779cf8cd1b
noirka <nuarveronika@gmail.com>
2025-11-29 15:43:08 +0200

    refactor(vite): Improve build performance

--- hanabi-online/backend/server.js
+++ hanabi-online/backend/server.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
    cors: {
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:5173',
       methods: ['GET', 'POST']
    }
 });
