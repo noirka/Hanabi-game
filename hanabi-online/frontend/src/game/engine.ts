0f2de7bfafca6166c4af77e97433cee1afeea7d4
noirka <nuarveronika@gmail.com>
2025-12-01 19:43:34 +0200

    feat: create GameEngine class skeleton

---
+++ hanabi-online/frontend/src/game/engine.ts
@@ -0,0 +1,46 @@
+import type { Player, GameSnapshot, Move } from "./types";
+import { generateDeck } from "./deck";
+import { deepClone } from "../utils/helpers";
+
+export class GameEngine {
+  players: Player[] = [];
+  deck = generateDeck();
+  discard = [];
+  fireworks = {
+    red: 0,
+    blue: 0,
+    green: 0,
+    yellow: 0,
+    white: 0,
+  };
+
+  hints = 8;
+  strikes = 0;
+  turn = 1;
+  currentPlayerIndex = 0;
+  finished = false;
+
+  constructor() {}
+
+  snapshot(): GameSnapshot {
+    return {
+      players: deepClone(this.players),
+      discard: deepClone(this.discard),
+      fireworks: deepClone(this.fireworks),
+      deckCount: this.deck.length,
+      hints: this.hints,
+      strikes: this.strikes,
+      turn: this.turn,
+      currentPlayerIndex: this.currentPlayerIndex,
+      finished: this.finished,
+    };
+  }
+
+  performMove(_move: Move) {
+    throw new Error("performMove() not implemented yet");
+  }
+
+  setup(_players: Player[]) {
+    this.players = _players;
+  }
+}
