c84ff5fb40be841c001eeffd9afb15d4b02a5e85
noirka <nuarveronika@gmail.com>
2025-12-08 10:10:25 +0200

    feat: Initialize and confirm WebSocket server connection

--- hanabi-online/backend/src/server.ts
+++ hanabi-online/backend/src/server.ts
@@ -2,20 +2,25 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import { WebSocketServer } from "ws";
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 
+const wss = new WebSocketServer({ server });
+
 const io = new Server(server, {
   cors: { origin: "*" }
 });
 
+// тест
 app.get("/", (_req, res) => {
   res.send("Hanabi backend is running");
 });
 
+// socket connection
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
@@ -26,4 +31,18 @@ io.on("connection", (socket) => {
 
 server.listen(5000, () => {
   console.log("Backend listening on http://localhost:5000");
+});
+
+wss.on("connection", (ws) => {
+  console.log("WS client connected");
+
+  ws.send("hello");
+
+  ws.on("message", (msg) => {
+    console.log("client says:", msg.toString());
+  });
+
+  ws.on("close", () => {
+    console.log("WS client disconnected");
+  });
 });
